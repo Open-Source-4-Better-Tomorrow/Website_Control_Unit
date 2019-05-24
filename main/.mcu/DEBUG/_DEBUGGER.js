4f7b64c4a32e55a721d895ffa12252ad6b557acc
Open Source 4 Better Tomorrow <o.s.4.b.t@gmail.com>
2019-05-24 14:32:46 +0200

    WCU [v1.0.0] | RC Update1 HotFix 1 : all paths relative to View/, DEBUGGER's logging improved, removal of unused event | 2019-05-24 2:33 PM Local Time

--- main/.mcu/DEBUG/_DEBUGGER.js
+++ main/.mcu/DEBUG/_DEBUGGER.js
@@ -1,25 +1,60 @@
 var _counter = 0;
 
-
 function getCurrentIteration() {
     return _counter;
 }
 
+function createPadding(message_string) {
+    var padding;
+
+    var message_available_width = 180;
+
+    var message_length = Math.abs(message_available_width - message_string.length);
+
+    padding = new Array(message_length + 1).join('.');
+
+    return message_string + padding;
+}
+
+function getCurrentLogTime() {
+    var today = new Date();
+
+    var logTime =   "| " +
+                    today.getDate() +"/" +
+                    (today.getMonth()+1)  + "/" +
+                    today.getFullYear() + " @ " +
+                    formatDatePart_I_1L(today.getHours(), 2, "0") + ":" +
+                    formatDatePart_I_1L(today.getMinutes(), 2, "0") + ":" +
+                    formatDatePart_I_1L(today.getSeconds(), 2, "0") + ":" +
+                    formatDatePart_I_1L(today.getMilliseconds(), 3, "0");
+
+    return logTime;
+
+
+
+    /**
+     * Local helper functions
+    */
+    function formatDatePart_I_1L(datepart, datepartWidth, datepartFiller) {
+        var milis = "";
+
+        if(datepart.toString().length < datepartWidth) {
+            for(var i = 0; i < datepartWidth - datepart.toString().length; i++)
+                milis += datepartFiller;
+        }
+
+        milis += datepart;
+
+        return milis;
+    }
+}
 
 
 var _debugger = {
     count : function(message) {
         var message_string = message + getCurrentIteration();
 
-        var currentdate = new Date();
-        var log = createPaddinng_I_1L(message_string) + "| " +
-                                                                currentdate.getDate() +"/" +
-                                                                (currentdate.getMonth()+1)  + "/" +
-                                                                currentdate.getFullYear() + " @ " +
-                                                                formatDatePart_I_1L(currentdate.getHours(), 2, "0") + ":" +
-                                                                formatDatePart_I_1L(currentdate.getMinutes(), 2, "0") + ":" +
-                                                                formatDatePart_I_1L(currentdate.getSeconds(), 2, "0") + ":" +
-                                                                formatDatePart_I_1L(currentdate.getMilliseconds(), 3, "0");
+        var log = createPadding(message_string) + getCurrentLogTime();
 
         console.log(log);
 
@@ -29,44 +64,18 @@ var _debugger = {
             var d = new Date();
             var e = d.getTimezoneOffset();
         }
-
-
-
-        /**
-         * Local helper functions
-        */
-
-        function createPaddinng_I_1L(message_string) {
-            var padding;
-
-            var message_available_width = 180;
-
-            var message_length = Math.abs(message_available_width - message_string.length);
-
-            padding = new Array(message_length + 1).join('.');
-
-            return message_string + padding;
-        }
-
-        function formatDatePart_I_1L(datepart, datepartWidth, datepartFiller) {
-            var milis = "";
-
-            if(datepart.toString().length < datepartWidth) {
-                for(var i = 0; i < datepartWidth - datepart.toString().length; i++)
-                    milis += datepartFiller;
-            }
-
-            milis += datepart;
-
-            return milis;
-        }
     },
 
     check : function(flag) {
         if(flag && _counter == 10)
             throw Error("This was examplary usage of current state of \"Website Control Unit, a.k.a Progressive Website Mechanism\"");
         else {
-            console.log("-----------------------------------------------------------------------------------------------------------------------");
+            var message_string = "-----------------------------------------------------------------------------------------------------------------------" + getCurrentIteration();
+
+            var log = createPadding(message_string) + getCurrentLogTime();
+
+            console.log(log);
+
             _counter++;
         }
     }
